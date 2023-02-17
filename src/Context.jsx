e8d3669231b951b6fe8caa400e9ce8ba07fc1880
martin-wrede <81897938+martin-wrede@users.noreply.github.com>
2023-02-17 12:32:40 +0100

    6h commit, error handling with async-await

--- src/Context.jsx
+++ src/Context.jsx
@@ -8,6 +8,9 @@ function ContextProvider({children}) {
     const [cartItems, setCartItems] = useState([])
     
     const url = "https://raw.githubusercontent.com/bobziroll/scrimba-react-bootcamp-images/master/images.json"
+    
+   // old error handling solution with promises 
+    /*
     useEffect(() => {
         fetch(url)
             .then(res => res.json())
@@ -20,6 +23,23 @@ function ContextProvider({children}) {
                 )
 
     }, [])
+    */
+
+    // new solution with async-await in useeffect
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await fetch(url);
+                const data = await response.json();
+                setAllPhotos(data);
+            } catch (error) {
+                alert(error);
+                // console.error(error);
+            }
+        };
+    
+        fetchData();
+    }, []);
     
     function toggleFavorite(id) {
         const updatedArr = allPhotos.map(photo => {
